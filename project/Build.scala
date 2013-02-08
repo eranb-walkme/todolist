1cb137665c0e540a19952719f6891c6dff8d754c
unknown <eran.b@walkme.com>
2013-02-08 12:58:51 +0200

    init

--- project/Build.scala
+++ project/Build.scala
@@ -8,6 +8,9 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
  val appDependencies = Seq(
+     javaCore,
+    javaJdbc,
+    javaEbean,
   	"postgresql" % "postgresql" % "8.4-702.jdbc4"
 	)
 
